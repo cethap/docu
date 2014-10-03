b865529907b03a11359a0de818c17e43f62f5c60
Cesar Augusto Tapasco <cethapgames@gmail.com>
2014-10-02 22:09:13 -0500

    Archivos nuevos

--- tests/test.js
+++
@@ -1,9 +0,0 @@
-/* jshint strict: true */
-describe("Test the app", function () {
-    'use strict';
-
-    // Test that true is true
-    it("tests that true is true", function () {
-        expect(true).toBe(true);
-    });
-});
